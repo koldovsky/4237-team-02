9629ff532dfa2acce9fda532ffca087adc0ba89d
ArtemNyk <artproger@outlook.com>
2025-04-15 08:06:02 +0300

    add anwer on task js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -28,5 +28,7 @@ function greet(name, owner) {
     return "Hello guest";
   }
 }
-
-
+//https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
