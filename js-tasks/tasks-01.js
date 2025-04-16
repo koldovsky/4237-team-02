44ce048ba19d1ef18755ccfed75188a948116f0a
Valeriia Remyha <lera.skorina@gmail.com>
2025-04-16 20:45:58 +0300

    Update tasks-01.js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -6,6 +6,16 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
 
 //https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 
+function makeNegative(num) {
+  if (num < 0) {
+    return num;
+  } else {
+    return num * -1;
+  }
+}
+
+//https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
 function move(position, roll) {
   return roll * 2 + position;
 }
