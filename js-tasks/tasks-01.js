3477ff54d588934c7b609f53ee4b2efb2fedc34f
Valeriia Remyha <lera.skorina@gmail.com>
2025-04-16 20:41:05 +0300

    Update tasks-01.js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -3,3 +3,10 @@
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
+
+//https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+function move(position, roll) {
+  return roll * 2 + position;
+}
+
