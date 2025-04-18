7366990c4c89a607a10dfa030dee82e48b446662
OlehLek <kaneki.ken.ukraine@gmail.com>
2025-04-18 17:42:03 +0300

    format tasks-01.js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -1,41 +1,29 @@
- // Lekhiv Oleh
- 
- //https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
- function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
- }
- 
- //https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
- function makeNegative(num) {
-   return -Math.abs(num);
- }
- 
- //https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
- function move (position, roll) {
-   return position + 2 * roll;
- }
- 
- //https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
- function greet (name, owner) {
-   return `Hello ${name === owner ? "boss" : "guest"}`
- }
- 
- // Lekhiv Oleh End
- 
- // Pavlo Naichuk
+//https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
 
- //https://www.codewars.com/kata/keep-hydrated-1/train/javascript
- function litres(time) {
-   return (time * 0.5) | 0;
- }
- 
- //https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
- function lovefunc(flower1, flower2) {
-   return flower1 % 2 !== flower2 % 2;
- }
- 
- //Pavlo Nauchuk
- 
- 
+//https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+function makeNegative(num) {
+  return -Math.abs(num);
+}
 
+//https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+function move(position, roll) {
+  return position + 2 * roll;
+}
 
+//https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+function greet(name, owner) {
+  return `Hello ${name === owner ? "boss" : "guest"}`;
+}
+
+//https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+function litres(time) {
+  return (time * 0.5) | 0;
+}
+
+//https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 !== flower2 % 2;
+}
