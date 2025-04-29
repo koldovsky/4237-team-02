17ca4f044b6579b5834205d9f130cabf63e33976
PavloNaichuk <pavlo.naichuk@gmail.com>
2025-04-29 12:16:44 +0300

    Added  logic  and js file history

---
+++ js/index.history.js
@@ -0,0 +1,28 @@
+function initHistoryAnimation() {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const block = entry.target;
+                block.classList.add('show');
+                observer.unobserve(block);
+            }
+        });
+    }, {
+        threshold: 0.2
+    });
+
+    const blocks = document.querySelectorAll('.history__timeline-block');
+
+    blocks.forEach((block, index) => {
+        block.style.transitionDelay = `${index * 150}ms`;
+
+        observer.observe(block);
+
+        if (block.getBoundingClientRect().top < window.innerHeight) {
+            block.classList.add('show');
+            observer.unobserve(block);
+        }
+    });
+}
+
+initHistoryAnimation();
